1baa90d96176be601ccfcad6ac63e3f7b492874a
robertpepelnjak <rpepelnjak@gmail.com>
2022-12-29 15:59:30 +0100

    krizanje?
    pregledovanje, ce se ladjica kriza

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import tkinter
 # A is between B and C
 #
 def between(a,b,c):
-  return (b <= a <= c) or (c <= a <= b)
+  return (b < a < c) or (c < a < b)
 
+def inclusive(a,b,c):
+  return (b <= a <= c) or (c <= a <= b)
 class Knofek:
   def __init__(self, x, y, button,frame):
     self.x = x
@@ -20,10 +22,17 @@ class Knofek:
   def risiLadjico(self):
     for y in range(10):
       for x in range(10):
-        if between(y ,self.drugiKonec.y, self.y) and between(x, self.drugiKonec.x, self.x):
-          tabela[y][x].tkButton.configure(highlightbackground="black", state = tkinter.DISABLED)
+        t = tabela[y][x]
+        if inclusive(y ,self.drugiKonec.y, self.y) and inclusive(x, self.drugiKonec.x, self.x):
+          t.tkButton.configure(highlightbackground="black", state = tkinter.DISABLED)
+          t.stanje = i
+        elif t.stanje != 0:
+          pass
         else:
-          tabela[y][x].tkButton.configure(state = tkinter.NORMAL)
+          t.tkButton.configure(
+            state = tkinter.NORMAL,
+            highlightbackground = "white",
+            command = t.klik)
 
 
 okno = tkinter.Tk()
@@ -34,7 +43,7 @@ for y in range(10):
   tabela.append([])
   for x in range(10):
     frame = tkinter.Frame(okno, width=40, height=40)
-    button = tkinter.Button(frame)
+    button = tkinter.Button(frame, highlightbackground="white")
     pametniKnofek = Knofek(x,y,button,frame)
     button.configure(command=pametniKnofek.klik)
     tabela[y].append(pametniKnofek)
@@ -49,14 +58,22 @@ def iskanjeladjice(l_x, l_y):
   for y in range(10):
     for x in range(10):
       t = tabela[y][x]
+      t.tkButton.configure(state = tkinter.DISABLED)
       if (t.x == l_x + i and t.y == l_y) \
           or (t.x == l_x - i and t.y == l_y) \
           or (t.y == l_y + i and t.x == l_x) \
           or (t.y == l_y - i and t.x == l_x):
-        t.drugiKonec = tabela[l_y][l_x]
-        t.tkButton.configure(highlightbackground="grey",command=t.risiLadjico)
-        
-      else:
-        tabela[y][x].tkButton.configure(state = tkinter.DISABLED)
+
+        krizanje = False
+        for vy in range(10):
+          for vx in range(10):
+            if between(vx,t.x,l_x) and between(vy,t.y,l_y) and tabela[vy][vx].stanje != 0:
+              krizanje = True
+
+        if not krizanje:
+          t.drugiKonec = tabela[l_y][l_x]
+          t.tkButton.configure(highlightbackground="grey",command=t.risiLadjico, state = tkinter.NORMAL)
 
 okno.mainloop()
+
+
